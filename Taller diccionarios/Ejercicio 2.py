688ee5262d1234fc1463b06d558591e783016867
Marianajar16 <109982206+Marianajar16@users.noreply.github.com>
2022-09-07 19:45:31 -0500

    Ejercicios resueltos

---
+++ Taller diccionarios/Ejercicio 2.py
@@ -0,0 +1,6 @@
+diccionario={'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 
+'Maite': 5} 
+lista=[]
+for key in diccionario:
+    lista.append(diccionario[key])
+print(list(set(lista)))
